ccba5ca91f4717fe4c9c6dfa8a8ec84540eef6ec
Jakub Marszal <marszal.kuba@gmail.com>
2022-12-11 23:54:58 +0100

    Day10 - Part 2 done

--- day10.js
+++ day10.js
@@ -3,6 +3,7 @@ const array = require('./day10-input.js');
 const ops = []
 let register = 1;
 let signal = 0;
+const CRT = [];
 
 const main = () => {
     let prev = 1;
@@ -20,9 +21,8 @@ const main = () => {
         }
     }
 
-    console.log(ops)
-
-    for(let i=1;i<=220;i++) {
+    let sprite = register;
+    for(let i=1;i<=240;i++) {
         const operation = ops.find(x => x.finish===i);
         if(operation!== undefined)
             register += operation.value;
@@ -30,9 +30,26 @@ const main = () => {
         if(i=== 20 || i=== 60 || i=== 100 || i=== 140 || i=== 180 || i=== 220) {
             signal += (i*register);
         }
-    }
 
+        const modifier = Math.floor(i/40)*40;
+        sprite = register + modifier;
+        const decision = sprite === i - 1 || sprite + 1 === i - 1 || sprite - 1 === i - 1;
+        if(decision) {
+            CRT[i-1]=1;
+        }
+        else {
+            CRT[i-1]=0;
+        }
+        console.log(i, register, sprite, decision)
+    }
+    
     console.log(signal)
+    console.log(JSON.stringify(CRT.splice(0,40)))
+    console.log(JSON.stringify(CRT.splice(0,40)))
+    console.log(JSON.stringify(CRT.splice(0,40)))
+    console.log(JSON.stringify(CRT.splice(0,40)))
+    console.log(JSON.stringify(CRT.splice(0,40)))
+    console.log(JSON.stringify(CRT.splice(0,40)))
 }
 
 main()
